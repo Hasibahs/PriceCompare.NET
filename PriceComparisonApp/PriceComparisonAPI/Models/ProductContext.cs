b8f207ce63addc9a5c64912f34d63cb7574efbc9
Timothy Adewale <adewale.adewale@student.nhlstenden.com>
2025-03-30 23:27:16 +0200

    Fetched the supermakets products into the db, created asnc and sync

---
+++ PriceComparisonApp/PriceComparisonAPI/Models/ProductContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PriceComparisonAPI.Models
+{
+    public class ProductContext : DbContext
+    {
+        // A DbSet for your Product entity
+        public DbSet<Product> Products => Set<Product>();
+
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
