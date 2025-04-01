eba05a5cca802781b6b9b7c4c5a6be704f0ac464
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2025-04-01 16:23:44 +0200

    front end

---
+++ PriceComparisonApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PriceComparisonApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
