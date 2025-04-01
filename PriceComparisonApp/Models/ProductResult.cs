c15068879cd9aa8aaa4d200fed45c56f04199b75
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2025-04-01 16:21:29 +0200

    front end updated

--- PriceComparisonApp/Models/ProductResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PriceComparisonApp.Models
-{
-    public class ProductResult
-    {
-        public string Store { get; set; } = string.Empty;
-        public double Price { get; set; }
-        public bool IsBestDeal { get; set; }
-    }
-}
